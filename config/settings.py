ddd59b3c17da0583eb60b1af382817a7e5ec3166
takuya2816 <taku0909.kishikishi@gmail.com>
2020-02-24 17:12:47 +0900

    chenge models.py

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accounts',  # アカウントappを登録
+    'accounts.apps.AccountsConfig',  # アカウントappを登録
 ]
 
 MIDDLEWARE = [
